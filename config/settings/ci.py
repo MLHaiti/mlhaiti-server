6b6dd586a232265b3110989a07efd44cd94f5c7b
Madsen Servius <madsen@madsendynamics.com>
2020-10-11 19:58:15 -0400

    secret key in settings ci

--- config/settings/ci.py
+++ config/settings/ci.py
@@ -1,14 +1,7 @@
-# In order to disallow django environ 
-# to read .env file in base settings
-# when travis are running
-CI = True 
-
-import os
-
 from .base import * 
 
 import dj_database_url
 
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY="#&3q+m3rfc7isd#7v+4yqm%obb*s10z)+rxs0^*tr-yx^6(l7m"
 
 DATABASES = {'default': dj_database_url.config()}
